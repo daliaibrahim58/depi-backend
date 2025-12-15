68324691e0a8be3d53b76ad13305b621843308ba
daliaibrahim58 <daliaibrahim582005@gmail.com>
2025-12-15 16:35:09 +0200

    edit cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
